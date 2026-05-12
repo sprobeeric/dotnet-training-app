ff16db1a4ad69d48f04717b329ebf4d871309e16
comahigcc <comahig.cc@sprobe.com>
2026-05-12 15:16:10 +0800

    Add sort icon

--- tests/DocumentTracker.Tests/Controllers/PaymentReceiptControllerTests.cs
+++ tests/DocumentTracker.Tests/Controllers/PaymentReceiptControllerTests.cs
@@ -13,7 +13,7 @@ public class PaymentReceiptControllerTests
     {
         var service = new Mock<IPaymentReceiptService>();
         service
-            .Setup(paymentReceiptService => paymentReceiptService.SearchAsync(null, null, null, 1, 10))
+            .Setup(paymentReceiptService => paymentReceiptService.SearchAsync(null, null, null, "payment_date_utc", "desc", 1, 10))
             .ReturnsAsync(new PaginatedResult<PaymentReceiptListItemViewModel>());
 
         var controller = new PaymentReceiptController(service.Object);
@@ -24,6 +24,6 @@ public class PaymentReceiptControllerTests
         var model = Assert.IsType<PaymentReceiptSearchViewModel>(viewResult.Model);
         Assert.Equal(1, model.Page);
         Assert.Equal(10, model.PageSize);
-        service.Verify(paymentReceiptService => paymentReceiptService.SearchAsync(null, null, null, 1, 10), Times.Once);
+        service.Verify(paymentReceiptService => paymentReceiptService.SearchAsync(null, null, null, "payment_date_utc", "desc", 1, 10), Times.Once);
     }
 }
