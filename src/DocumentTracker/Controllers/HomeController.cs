e07d40f5106084079fbd0786d2ad19efc4f03ea2
carinojk <carino.jk@sprobe.com>
2026-05-14 17:27:55 +0800

    handle invalid/non-existing invoice IDs are handled properly

--- src/DocumentTracker/Controllers/HomeController.cs
+++ src/DocumentTracker/Controllers/HomeController.cs
@@ -8,12 +8,26 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
-        return RedirectToAction("Index", "Documents");
+        return RedirectToAction("Index", "Invoices");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [Route("Home/StatusCode/{statusCode:int}")]
+    public IActionResult StatusCode(int statusCode)
+    {
+        Response.StatusCode = statusCode;
+
+        if (statusCode == StatusCodes.Status404NotFound)
+        {
+            return View("NotFound");
+        }
+
+        return View("Error");
     }
 }
